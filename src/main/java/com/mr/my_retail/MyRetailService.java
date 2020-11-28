8230c01527269e991ff80f52285a89d0189eebe2
Prasad Bayi <74462843+prasadbayi@users.noreply.github.com>
2020-11-29 04:42:10 +0530

    code quality enhancement fixed.

--- src/main/java/com/mr/my_retail/MyRetailService.java
+++ src/main/java/com/mr/my_retail/MyRetailService.java
@@ -15,49 +15,50 @@ public class MyRetailService {
 
 	@Value("${myapp.catalog.endpoint}")
 	private String catalogEndPoint;
-	
+
 	@Value("${myapp.pricing.endpoint}")
 	private String pricingEndPoint;
-	
+
 	public MyRetailService() {
 		restTemplate = new RestTemplate();
 	}
-	
+
 	public Product getProducts(Long id) {
-		//Example response: {"id":13860428,"name":"The Big Lebowski (Blu-ray) (Widescreen)","current_price":{"value": 13.49,"currency_code":"USD"}}
+		// Example response: {"id":13860428,"name":"The Big Lebowski (Blu-ray)
+		// (Widescreen)","current_price":{"value": 13.49,"currency_code":"USD"}}
 		log.debug("MyRetailService.getProducts():: Fetching Proudct details for id = " + id);
-	
+
 		Product responseBody = getProduct(id);
-		Product.current_price currentPrice = getCurrentPrice(responseBody.getId());
-		responseBody.setCurrent_price(currentPrice);
+		if (responseBody != null) {
+			Product.current_price currentPrice = getCurrentPrice(responseBody.getId());
+			responseBody.setCurrent_price(currentPrice);
+		}
 		return responseBody;
 	}
-	
+
 	private Product getProduct(Long id) {
-		String urlString = catalogEndPoint+id;
+		String urlString = catalogEndPoint + id;
 		String endpoint = "";
 		try {
 			URL url = new URL(urlString);
 			endpoint = url.toString();
-		}
-		catch(Exception e) {
-			log.error("MyRetailService.getProduct()"+ e.getStackTrace().toString());
+		} catch (Exception e) {
+			log.error("MyRetailService.getProduct()" + e.getStackTrace().toString());
 		}
 		Product responseBody = restTemplate.getForObject(endpoint, Product.class);
-		return responseBody;	
+		return responseBody;
 	}
-	
+
 	private Product.current_price getCurrentPrice(Long id) {
-		String urlString = pricingEndPoint+id;
+		String urlString = pricingEndPoint + id;
 		String endpoint = "";
 		try {
 			URL url = new URL(urlString);
 			endpoint = url.toString();
-		}
-		catch(Exception e) {
-			log.error("MyRetailService.getCurrentPrice()"+ e.getStackTrace().toString());
+		} catch (Exception e) {
+			log.error("MyRetailService.getCurrentPrice()" + e.getStackTrace().toString());
 		}
 		final Product.current_price current_price = restTemplate.getForObject(endpoint, Product.current_price.class);
-		return current_price;	
+		return current_price;
 	}
 }
